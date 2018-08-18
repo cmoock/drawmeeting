31de0f3a554887c2180d361158f234aa673111ce
Colin <colin@moock.org>
2018-08-17 20:33:33 -0400

    -Initial implementation of saving a drawing to the server. Will change when format is changed to JSON from form-urlencoded.

--- gallery.js
+++ gallery.js
@@ -3,9 +3,11 @@
 
   function init () {
     console.log("Gallery init()")
-    document.getElementById("saveDrawingBtn").addEventListener("click", function () {
-      getScreenshot();
-    });
+    dm.addClickListener(document.getElementById("saveDrawingBtn"), saveClickListener);
+  }
+
+  function saveClickListener () {
+    saveDrawingToServer(canvas.toDataURL("image/png"));
   }
 
   function getScreenshot () {
@@ -19,6 +21,25 @@
     img.style["max-height"] = "100%";
     img.style["margin-top"] = "5px";
     gallery.insertBefore(img, gallery.firstChild);
+  }
+
+  function saveDrawingToServer (canvasData) {
+    var request = new XMLHttpRequest();
+    var url = 'https://drawmeeting-service.herokuapp.com/screenshots';
+    // var payload = JSON.stringify({data: canvasData});
+    var payload = "data=" + canvasData;
+    request.open('POST', url, true);
+
+    //Send the proper header information along with the request
+    //request.setRequestHeader('Content-type', 'application/json');
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        console.log(request.responseText);
+      }
+    }
+    request.send(payload);
   }
 
 })();

48171d408aca6bf89bea7b021bc23f119a929460
Colin <colin@moock.org>
2018-08-22 20:04:14 -0400

    -Basic display of loaded drawings functional. CORS issues resolved.

--- gallery.js
+++ gallery.js
@@ -3,6 +3,8 @@
 
   function init () {
     console.log("Gallery init()")
+    API_URL = "http://api.drawmeeting.com/"
+
     dm.addClickListener(document.getElementById("saveDrawingBtn"), saveClickListener);
     loadDrawings();
   }
@@ -28,16 +30,16 @@
     var request = new XMLHttpRequest();
     var url = 'http://api.drawmeeting.com/screenshots';
     // var payload = JSON.stringify({data: canvasData});
-    var payload = "data=" + canvasData;
+    var payload = "data=" + encodeURIComponent(canvasData);
     request.open('POST', url, true);
 
+    console.log("CANVAS STRING: " + canvasData)
     //Send the proper header information along with the request
     //request.setRequestHeader('Content-type', 'application/json');
     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
     request.onreadystatechange = function() {
       if (request.readyState == 4 && request.status == 200) {
-        console.log(request.responseText);
       }
     }
     request.send(payload);
@@ -45,11 +47,28 @@
 
   function loadDrawings () {
     var request = new XMLHttpRequest();
-    var url = 'https://drawmeeting-service.herokuapp.com/screenshots';
+    var url = API_URL + 'screenshots';
     request.open('GET', url, true);
     request.onreadystatechange = function() {
       if (request.readyState == 4 && request.status == 200) {
-        console.log(request.responseText);
+        var drawings = JSON.parse(request.responseText);
+        console.log(drawings)
+
+        var gallery = document.getElementById("gallery");
+        var img;
+
+        console.log("SERVER STRING: " +  drawings[drawings.length-1].data)
+
+        for (var i = 0; i < drawings.length; i++) {
+          img = new Image();
+          img.onerror = function() {console.log("Error attempting to parse gallery image: " + i)};
+          img.onabort = function() {console.log("Aborted loading image: " + i)};
+          img.src = drawings[i].data;
+          img.style["max-width"] = "100%";
+          img.style["max-height"] = "100%";
+          img.style["margin-top"] = "5px";
+          gallery.insertBefore(img, gallery.firstChild);
+        }
       }
     }
     request.send();
